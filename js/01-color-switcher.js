18adb9136d34845606422f5bd0c54113db8d9068
justjoannams <justjoannams@gmail.com>
2024-05-31 17:36:59 +0800

    goit-js-hw-09-task-01

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,41 @@
+const bodyEl = document.querySelector("body");
+const startEl = document.querySelector("button[data-start]");
+const stopEl = document.querySelector("button[data-stop]");
+stopEl.disabled = true;
+let intervalID = null;
 
+const randomBodyColorGenerator = {
+  DELAY: 1000,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    intervalID = setInterval(() => {
+      changeBgColorRandom();
+    }, this.DELAY);
+    stopEl.disabled = false;
+  },
+
+  start() {
+    startEl.addEventListener("click", () => {
+      this.interval();
+      startEl.disabled = true;
+      stopEl.disabled = false;
+    });
+    stopEl.addEventListener("click", this.stop);
+  },
+
+  stop() {
+    clearInterval(intervalID);
+    stopEl.disabled = true;
+    startEl.disabled = false;
+  },
+};
+
+function changeBgColorRandom() {
+  bodyEl.style.background = randomBodyColorGenerator.getRandomHexColor();
+}
+
+randomBodyColorGenerator.start();
